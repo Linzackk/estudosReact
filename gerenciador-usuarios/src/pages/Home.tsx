a8db1f500156bd1b213e36c76e5df20de2929bd2
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-01-15 15:18:18 -0300

    feat criacao do hook useFavorites

--- gerenciador-usuarios/src/pages/Home.tsx
+++ gerenciador-usuarios/src/pages/Home.tsx
@@ -1,25 +1,37 @@
 import React, { useCallback } from "react";
 import UserCard from "../components/UserCard";
-import { useUsers } from "../hooks/useUsers";
+import { useUsers, User } from "../hooks/useUsers";
+import { useFavorites } from "../hooks/useFavorites";
 
 const Home: React.FC = () => {
+    const { users } = useUsers();
+    const { addFavorite, favorites, removeFavorite } = useFavorites();
+
     const favoriteUser = useCallback(() => {
         console.log("Usuario favoritado")
     }, []);
 
-    const { users } = useUsers();
+    const toggleFavorite = useCallback(
+        (user: User) => {
+            if (favorites.find((u) => u.id === user.id)) {
+                removeFavorite(user.id)
+            } else {
+                addFavorite(user);
+            }
+        },
+        [favorites, addFavorite, removeFavorite]
+    );
 
     return (
         <div>
             <h1>Home - Usuários</h1>
-
             <ul>
                 {users.map((user) => (
                     <UserCard 
                         key={user.id} 
                         id={user.id} 
                         name={user.name} 
-                        onFavorite={favoriteUser} 
+                        onFavorite={() => toggleFavorite(user)} 
                     />
                 ))}
             </ul>
